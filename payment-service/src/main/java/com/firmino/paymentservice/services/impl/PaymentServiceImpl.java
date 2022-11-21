9921e586ffac959c64da89311380d1c0434906c3
Filipe Firmino <filipefirmino@gec.inatel.br>
2022-11-21 12:28:20 -0300

    add producer config payment-service

--- payment-service/src/main/java/com/firmino/paymentservice/services/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/firmino/paymentservice/services/impl/PaymentServiceImpl.java
@@ -2,15 +2,27 @@ package com.firmino.paymentservice.services.impl;
 
 import com.firmino.paymentservice.model.Payment;
 import com.firmino.paymentservice.services.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+
+@RequiredArgsConstructor
 @Service
 @Log4j2
 public class PaymentServiceImpl implements PaymentService {
 
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
        log.info("PAYMENT-SERVICE-IMPL ::: Pagamento {}", payment);
+       Thread.sleep(1000);
+
+       log.info("Enviando pagamento...");
+       kafkaTemplate.send("payment-topic", payment);
     }
 }
